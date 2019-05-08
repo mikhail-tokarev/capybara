6792ea7bf89282ea82ed6ef120d3fc3ab9453b3f
Mikhail Tokarev <mikhail.tokarev@gmail.com>
2019-05-08 13:50:12 +0300

    typo

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title, style: TextStyle(fontFamilty: 'Raleway')),
+        title: new Text(widget.title, style: TextStyle(fontFamily: 'Raleway')),
       ),
       body: new Center(
         // Center is a layout widget. It takes a single child and positions it
