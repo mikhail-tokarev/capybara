9e7f53bb9c4a41d671ad968323ac4fd01aa53b5a
Mikhail Tokarev <mikhail.tokarev@gmail.com>
2019-05-08 12:28:00 +0300

    add custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter CI',
+      title: 'Codemagic Demo',
       theme: new ThemeData(
         // This is the theme of your application.
         //
