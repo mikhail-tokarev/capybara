57a7bc9102daab525f69f1c0cc59fb1642a59c66
Mikhail Tokarev <mikhail.tokarev@gmail.com>
2020-01-21 15:12:50 +0200

    recreate app

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/priit/Library/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/priit/AndroidStudioProjects/capybara"
+export "FLUTTER_ROOT=/Users/tokarev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tokarev/work/capybara"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/priit/Library/flutter/bin/cache/artifacts/engine/ios-release"
-export "FLUTTER_BUILD_NAME=1.0.2"
-export "FLUTTER_BUILD_NUMBER=3"
+export "FLUTTER_FRAMEWORK_DIR=/Users/tokarev/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
